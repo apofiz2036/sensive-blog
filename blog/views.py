a2dccb43944f89eb9ed70d877e7e6e25851cf597
Yuriy Mikhailov <apofiz2036@bk.ru>
2024-12-09 22:14:40 +0500

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -30,16 +30,19 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
+        'tags': {tag.title: post.tags_count for tag in post.tags.all()},
+        'first_tag_title': post.tags.first().title if post.tags.exists() else None,
     }
+#  [serialize_tag(tag) for tag in post.tags.all()]
+#  str(post.tags_count),
 
 
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': len(Post.objects.filter(tags=tag))
     }
+#         'posts_with_tag': len(Post.objects.filter(tags=tag))
 
 
 def index(request):
@@ -50,10 +53,11 @@ def index(request):
         .prefetch_related("author")[:5]
         .fetch_with_comments_count()
     )
+    print(most_popular_posts)
 
     fresh_posts = (
         Post.objects
-        .annotate(comments_count=Count('comments'))
+        .annotate(comments_count=Count('comments'), tags_count=Count('tags'))
         .order_by('published_at')
         .prefetch_related("tags")
         .prefetch_related('author')
