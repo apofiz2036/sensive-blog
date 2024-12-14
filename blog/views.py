a13d67bcaff4709dcb201a81a35a23669a0babf0
Yuriy Mikhailov <apofiz2036@bk.ru>
2024-12-14 18:25:49 +0500

    исправил замечания

--- blog/views.py
+++ blog/views.py
@@ -58,9 +58,7 @@ def index(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(
-        Post.objects.prefetch_related(
-            Prefetch('comments', queryset=Comment.objects.select_related('author'))
-        ).annotate(tags_count=Count('tags')),
+        Post.objects.prefetch_related('comments__author').annotate(tags_count=Count('tags')),
         slug=slug
     )
 
@@ -78,7 +76,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': post.likes,
+        'likes_amount': post.likes.count(),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
